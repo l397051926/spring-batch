c5f578540783e298aa56ac3b119beaf530557064
liumingxin@gennlife.com <18645484821@163.com>
2018-03-21 19:59:33 +0800

    定义域对象

--- src/main/java/com/gennlife/example/ch02/CreditBill.java
+++ src/main/java/com/gennlife/example/ch02/CreditBill.java
@@ -6,4 +6,10 @@ package com.gennlife.example.ch02;
  * @desc  信用卡消费记录领域对象
  **/
 public class CreditBill {
+
+    private String accountID = "";  //银行卡账户
+    private String name ="";    //持卡人姓名
+    private double amount = 0;  //消费金额
+    private String date;    //消费日期
+    private String address; //消费场所
 }
